7dbba6e35ef7a23a6002ef670dbb93fc5df1ea06
buingochuy124 <buingochuy124@gmail.com>
2022-05-05 15:00:38 +0700

    Add project files.

---
+++ DataAccess/ConnectDB.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class ConnectDB
+    {
+        public static SqlConnection GetSqlConnection()
+        {
+            SqlConnection sqlconn = null;
+            try
+            {
+                var connectName = "Server=.;Database=FPTLibraryManagement;Trusted_Connection=True;Max Pool Size=400";
+
+                sqlconn = new SqlConnection(connectName);
+
+                if (sqlconn.State == System.Data.ConnectionState.Open)
+                {
+                    sqlconn.Close();
+                }
+
+                sqlconn.Open();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return sqlconn;
+
+        }
+    }
+}
---
+++ FPTLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FPTLibrary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FPTLibrary")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("647fdd2f-81c4-43bf-a955-5806fe40c1a3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
