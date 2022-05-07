71d4e46ce5bd2c3b09b85eb981c6b30054bab698
binh <pbinh.2501@gmail.com>
2022-05-07 10:22:28 +0700

    storeDTO

---
+++ DataAccess/DTO/StoreDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DTO
+{
+    public class StoreDTO
+    {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public string StoreName { get; set; }
+
+    }
+}
